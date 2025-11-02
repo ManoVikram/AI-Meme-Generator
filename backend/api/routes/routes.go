75b4a61330a3778f548748b60d93b7d25dbfffad
Mano Vikram <manovik18@gmail.com>
2025-11-02 19:33:21 +0530

    Added route endpoint and passing services via dependency injection

--- backend/api/routes/routes.go
+++ backend/api/routes/routes.go
@@ -1,5 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/ManoVikram/AI-Meme-Generator/backend/api/handlers"
+	"github.com/ManoVikram/AI-Meme-Generator/backend/api/services"
+	"github.com/gin-gonic/gin"
+)
 
-func RegisterRoutes(server *gin.Engine) {}
+func RegisterRoutes(server *gin.Engine, services *services.Services) {
+	// GET method to generate meme image with a caption based on a topic
+	server.GET("/api/generate", handlers.GenerateMemeHandler(services))
+}
