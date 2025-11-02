f5a705f165303deb97869cc2a92c6b82c3a3767a
Mano Vikram <manovik18@gmail.com>
2025-11-02 19:34:09 +0530

    Updated the code to get the Python gRPC address from the environment file

--- backend/api/main.go
+++ backend/api/main.go
@@ -5,18 +5,30 @@ import (
 	"os"
 
 	"github.com/ManoVikram/AI-Meme-Generator/backend/api/routes"
+	"github.com/ManoVikram/AI-Meme-Generator/backend/api/services"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/ManoVikram/AI-Meme-Generator/backend/api/proto"
 )
 
 func main() {
-	// Step 1 - Initialize and connect to the Python gRPC server
-	connection, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Step 1 - Connect to the Python gRPC server
+	grpcAddress := os.Getenv("GRPC_SERVER_ADDRESS")
+	if grpcAddress == "" {
+		grpcAddress = "localhost:50051"
+	}
+	connection, err := grpc.NewClient(grpcAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect to gRPC server: %v", err)
 	}
 	defer connection.Close()
+
+	// Step 2 - Initialize the AI service client
+	aiServicesClient := &services.Services{
+		AIClient: pb.NewAIMemeGeneratorServiceClient(connection),
+	}
 
 	// Step 2 - Initialize and set up the Gin server
 	server := gin.Default()
@@ -24,7 +36,7 @@ func main() {
 	server.RedirectTrailingSlash = true
 
 	// Step 3 - Register the routes to the Gin server
-	routes.RegisterRoutes(server)
+	routes.RegisterRoutes(server, aiServicesClient)
 
 	// Step 4 - Start the Gin server
 	port := os.Getenv("PORT")
