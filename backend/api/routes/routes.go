6b506c684420bfa510b1e1caf6a14fbe485ded87
Mano Vikram <manovik18@gmail.com>
2025-11-04 00:44:09 +0530

    Updated the endpoint to use POST instead of GET

--- backend/api/routes/routes.go
+++ backend/api/routes/routes.go
@@ -8,5 +8,5 @@ import (
 
 func RegisterRoutes(server *gin.Engine, services *services.Services) {
 	// GET method to generate meme image with a caption based on a topic
-	server.GET("/api/generate", handlers.GenerateMemeHandler(services))
+	server.POST("/api/generate", handlers.GenerateMemeHandler(services))
 }
